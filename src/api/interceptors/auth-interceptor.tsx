6301c56e5ee107f41b314e1715a56fdbf6f520eb
“Thibault <thibault.gobert@inetum-realdolmen.world>
2025-07-10 16:00:00 +0200

    Develop boilerplate

--- src/api/interceptors/auth-interceptor.tsx
+++ src/api/interceptors/auth-interceptor.tsx
@@ -1,35 +1,35 @@
-// import axiosInstance from "../base-api";
+import axiosInstance from "../base-api";
 
-// // Add Auth interceptor
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
+// Add Auth interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-//     if (
-//       error.response &&
-//       error.response.status === 401 &&
-//       !originalRequest._retry
-//     ) {
-//       originalRequest._retry = true;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
 
-//       try {
-//         // Attempt silent refresh
-//         await axiosInstance.post(
-//           "/auth/refresh",
-//           {},
-//           { withCredentials: true }
-//         );
+      try {
+        // Attempt silent refresh
+        await axiosInstance.post(
+          "/auth/refresh",
+          {},
+          { withCredentials: true }
+        );
 
-//         // Retry the original request after successful refresh
-//         return axiosInstance(originalRequest);
-//       } catch (refreshError) {
-//         // Refresh failed – reroute to login
-//         window.location.href = "/login";
-//         return Promise.reject(refreshError);
-//       }
-//     }
+        // Retry the original request after successful refresh
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        // Refresh failed – reroute to login
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/")({
-  component: IndexRoute,
-});
-
-function IndexRoute() {
-  return <div className="p-2">Welcome Home</div>;
-}
